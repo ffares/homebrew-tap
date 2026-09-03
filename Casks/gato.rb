6939d9e16c9569c24f1089cca5fb0ecbbda83ac4
Fares <fares@fares.net>
2026-09-03 18:14:31 +0400

    gato: serve the disk image from GitHub releases

--- Casks/gato.rb
+++ Casks/gato.rb
@@ -2,7 +2,7 @@ cask "gato" do
   version "1.0.10"
   sha256 "f22dc971522d0496aa84a2d39f1cc1cf9642b74ca35f48949d3a5a61f69b2aba"
 
-  url "https://fares.net/gato/gato-#{version}.dmg"
+  url "https://github.com/ffares/gato-app/releases/download/v#{version}/gato-#{version}.dmg"
   name "gato"
   desc "Native macOS terminal with tabs, splits, SSH logins and a widget panel"
   homepage "https://fares.net/gato/"
